73357bf08fe9b2e3476c695634c7af3a4f7bf278
Boateng Pomaa Linda <lindapomaa27@gmail.com>
2023-09-13 08:38:08 +0100

    fixed get

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 import { addUser,getUser,updateUser,deleteUser,getAllUsers } from '../controller/userController.js'
 
 router.post('/api',addUser)
-.get('/api/:user_id',getUser)
+.get('/api/:name',getUser)
 .put('/api/:user_id',updateUser)
 .delete('/api/:user_id',deleteUser)
 .get('/api',getAllUsers)
